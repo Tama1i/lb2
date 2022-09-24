598733a4d8d06fc7a98bf06f05e28527c68b59eb
Tama1i <ryazantseff.matvei@yandex.ru>
2022-09-24 10:05:49 +0300

    changs in READ.mdc

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c,a = 10, b = 5;
+            c = a + b;
+            Console.Write(c);
+        }
+    }
+}
