bf40f01c5da4a006334a63b08d27e2dfc0f760e3
Tama1i <ryazantseff.matvei@yandex.ru>
2022-09-24 10:41:37 +0300

    6 com'

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleApp2
         {
             int c,a = 10, b = 5;
             c = a + b+1;
-            c += 4;
+            c += 5;
             Console.Write(c);
         }
     }
